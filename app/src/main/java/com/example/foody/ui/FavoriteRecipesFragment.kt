abbd42f54a3109284f593c2cebfac26dd55a236a
Rubens Alvares <rubens.alvares@aurum.com.br>
2023-10-25 17:31:07 -0300

    Fix navigation in bottom bar

--- app/src/main/java/com/example/foody/ui/FavoriteRecipesFragment.kt
+++ app/src/main/java/com/example/foody/ui/FavoriteRecipesFragment.kt
@@ -1,18 +1,25 @@
 package com.example.foody.ui
-
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.example.foody.R
+import com.example.foody.databinding.FragmentRecipesBinding
 
 class FavoriteRecipesFragment : Fragment() {
+
+    private var _binding : FragmentRecipesBinding? = null
+    private val binding get() = _binding!!
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        return inflater.inflate(R.layout.fragment_favorite_recipes, container, false)
-    }
+    ): View {
+        _binding = FragmentRecipesBinding.inflate(inflater, container, false)
 
+        return binding.root
+    }
+    override fun onDestroy() {
+        super.onDestroy()
+        _binding = null
+    }
 }
--- app/src/main/java/com/example/foody/ui/FoodJokeFragment.kt
+++ app/src/main/java/com/example/foody/ui/FoodJokeFragment.kt
@@ -1,27 +1,26 @@
 package com.example.foody.ui
-
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.example.foody.databinding.FragmentRecipesBinding
+import com.example.foody.databinding.FragmentFoodJokeBinding
 
-class RecipesFragment : Fragment() {
+class FoodJokeFragment : Fragment() {
 
-    private var _binding : FragmentRecipesBinding?= null
+    private var _binding : FragmentFoodJokeBinding? = null
     private val binding get() = _binding!!
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        _binding = FragmentRecipesBinding.inflate(inflater,  container, false)
+        _binding = FragmentFoodJokeBinding.inflate(inflater, container, false)
         return binding.root
     }
-
     override fun onDestroy() {
         super.onDestroy()
         _binding = null
     }
+
 }
